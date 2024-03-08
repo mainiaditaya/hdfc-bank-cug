c79bb0422c8c7a308bfc7ea49d41ffc4126a6596
Suparna Bose <45137253+suparnabose373@users.noreply.github.com>
2024-03-08 12:02:40 +0530

    Stepper for yourDetails screen (#21)
    * Stepper Styles
    
    ---------
    
    Co-authored-by: Suparna Bose <suparnab@adobe.com>

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -12,11 +12,30 @@ function getFullName(firstname, lastname) {
   lastname = (lastname == null) ? '' : lastname;
   return firstname.concat(' ').concat(lastname);
 }
+
+/**
+ * Decorates the stepper for CC yourDetails panel
+ * @name decorateStepper Runs after yourDetails panel is initialized
+ */
+function decorateStepper() {
+  const ccDetailsWizard = document.querySelector('.form-corporatecardwizardview.field-wrapper.wizard');
+
+  const totalIndex = ccDetailsWizard.style.getPropertyValue('--wizard-step-count');
+  Array.from(ccDetailsWizard.children).forEach((child) => {
+    if (child.tagName.toLowerCase() === 'fieldset' && (Number(child.style.getPropertyValue('--wizard-step-index')) !== totalIndex - 1)) {
+      const stepperLegend = document.querySelector(`main .form .form-corporatecardwizardview.field-wrapper.wizard .${child.className.split(' ').join('.')} > legend`);
+      stepperLegend?.classList?.add('stepper-style');
+    }
+  });
+}
+
 /**
  * On Wizard Init.
  * @name onWizardInit Runs on initialization of wizard
  */
-function onWizardInit() {}
+function onWizardInit() {
+  decorateStepper();
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export { getFullName, onWizardInit };
