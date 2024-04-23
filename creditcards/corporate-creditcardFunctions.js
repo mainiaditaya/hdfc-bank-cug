742b32fef9efa2d260e2b51a05c3de66ae152979
srivastava21ashish <srivastava21ashish@gmail.com>
2024-04-23 14:38:28 +0530

    Revert "name sanitization"
    This reverts commit d36d6fbaa3c9fe8131cdecab403849b0d1b1af88.

--- creditcards/corporate-creditcardFunctions.js
+++ creditcards/corporate-creditcardFunctions.js
@@ -228,15 +228,11 @@ const splitName = (fullName) => {
   const name = { firstName: '', middleName: '', lastName: '' };
   if (fullName) {
     const parts = fullName.split(' ');
-    name.firstName = sanitizeName(parts.shift()) || '';
-    name.lastName = sanitizeName(parts.pop()) || '';
-    name.middleName = sanitizeName(parts.length > 0 ? parts[0] : '');
+    name.firstName = parts.shift() || '';
+    name.lastName = parts.pop() || '';
+    name.middleName = parts.length > 0 ? parts[0] : '';
   }
   return name;
-};
-
-const sanitizeName = (name) => {
-  return name.replace(/[^a-zA-Z]/g, '');
 };
 
 /* Automatically fills form fields based on response data.
