962b3a76c3798b28d6fb1b43852e38388716b5b6
kunal-adobe <kunalj.adobe@gmail.com>
2024-07-18 13:29:56 +0530

    Commented page load analytic call

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,9 +1,9 @@
 import { sampleRUM } from './aem.js';
-import { corpCreditCardContext } from '../common/journey-utils.js';
-import { sendAnalytics } from '../common/analytics.js';
+// import { corpCreditCardContext } from '../common/journey-utils.js';
+// import { sendAnalytics } from '../common/analytics.js';
 
-const { currentFormContext } = corpCreditCardContext;
+// const { currentFormContext } = corpCreditCardContext;
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-if (!currentFormContext.journeyId) sendAnalytics('page load', {}, 'ACQUIRED', currentFormContext);
+// if (!currentFormContext.journeyId) sendAnalytics('page load', {}, 'ACQUIRED', currentFormContext);
 // add more delayed functionality here
