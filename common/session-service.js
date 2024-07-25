364026f9d0638674935fb5806d7a469a4e7d428f
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-07-25 16:00:18 +0530

    sesesion service payload request created

--- common/session-service.js
+++ common/session-service.js
@@ -1,20 +1,37 @@
 import corpCreditCard from './constants.js';
 import { urlPath } from './formutils.js';
 import { getJsonResponse } from './makeRestAPI.js';
+import { corpCreditCardContext } from './journey-utils.js';
 
 const { journeyInit } = corpCreditCard.endpoints;
+
+const createSessionPayload = async (globals) => {
+  try {
+    const jsonObjForSessionApi = {
+      requestString: {
+        jid: globals.form.runtime.journeyId.$value || globals.functions.exportData()?.currentFormContext.journeyId || corpCreditCardContext.currentFormContext.journeyId,
+        browserFingerPrint: '',
+        clientIp: '',
+        payloadEncrypted: '',
+      },
+    };
+    return jsonObjForSessionApi;
+  } catch (error) {
+    throw new Error('Failed to create document payload', error);
+  }
+};
 
 /**
  * calling journey session init api.
  * @name initSession
- * @param {object} reqPayload - request payload
  * @param {object} eventHandlers - event handler of success & error in object.
  */
-async function initSession(reqPayload, eventHandlers) {
+async function initSession(eventHandlers, globals) {
   const url = urlPath(journeyInit);
   const method = 'POST';
+  const payload = await createSessionPayload(globals);
   try {
-    const responseObj = await getJsonResponse(url, reqPayload, method);
+    const responseObj = await getJsonResponse(url, payload, method);
     if ((responseObj !== null) && ((responseObj.statusCode === 'SM00') || (responseObj.statusCode === 'SM01'))) {
       eventHandlers.successCallBack(responseObj);
     } else {
