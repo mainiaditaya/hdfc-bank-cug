3d9151b09765e2cde68fdd79b978670b42beff17
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-07-26 16:09:39 +0530

    removed unwanted code

--- common/functions.js
+++ common/functions.js
@@ -115,8 +115,6 @@ function checkMode(globals) {
         globals,
       );
     }
-    // currentFormContext.action = 'confirmation';
-    // sendPageloadEvent('CONFIRMATION_JOURNEY_STATE', globals);
   } if ((idcomVisit === 'DebitCard') || (idcomVisit === 'CreditCard')) { // debit card or credit card flow
     const resultPanel = formUtil(globals, globals.form.resultPanel);
     resultPanel.visible(false);
@@ -130,15 +128,6 @@ function checkMode(globals) {
     globals.functions.setProperty(globals.form.confirmResult, { visible: false });
     const userRedirected = true;
     executeInterfacePostRedirect('idCom', userRedirected, globals);
-  }
-  if (!formData?.form?.login?.registeredMobileNumber) {
-    globals.functions.setProperty(globals.form.loginPanel, { visible: false });
-    globals.functions.setProperty(globals.form.welcomeText, { visible: false });
-    globals.functions.setProperty(globals.form.getOTPbutton, { visible: false });
-    globals.functions.setProperty(globals.form.consentFragment, { visible: false });
-    globals.functions.setProperty(globals.form.resultPanel, { visible: true });
-    globals.functions.setProperty(globals.form.resultPanel.errorResultPanel, { visible: true });
-    invokeJourneyDropOff('CRM_LEAD_FAILURE', '9999999999', globals);
   }
   if (!formData?.form?.login?.registeredMobileNumber) {
     globals.functions.setProperty(globals.form.loginPanel, { visible: false });
