1e29451889e0df8b144f93de6f1f46ef74ca1f67
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2024-08-01 00:02:30 +0530

    Optimised deep clone github co-pilot

--- blocks/form/rules/model/afb-runtime.js
+++ blocks/form/rules/model/afb-runtime.js
@@ -164,23 +164,27 @@ const isCaptcha = function (item) {
     return fieldType === 'captcha';
 };
 function deepClone(obj, idGenerator) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
+    }
+
     let result;
-    if (obj instanceof Array) {
-        result = [];
-        result = obj.map(x => deepClone(x, idGenerator));
-    }
-    else if (typeof obj === 'object' && obj !== null) {
+    if (Array.isArray(obj)) {
+        result = new Array(obj.length);
+        for (let i = 0; i < obj.length; i++) {
+            result[i] = deepClone(obj[i], idGenerator);
+        }
+    } else {
         result = {};
-        Object.entries(obj).forEach(([key, value]) => {
-            result[key] = deepClone(value, idGenerator);
-        });
+        for (const key of Object.keys(obj)) {
+            result[key] = deepClone(obj[key], idGenerator);
+        }
     }
-    else {
-        result = obj;
-    }
+
     if (idGenerator && result && result.id) {
         result.id = idGenerator();
     }
+
     return result;
 }
 const jsonString = (obj) => {
