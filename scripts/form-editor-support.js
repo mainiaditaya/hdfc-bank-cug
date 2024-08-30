efcbb8e8fc4470f674f89b2360ac4c842adadef1
Nitin Gupta <nitigupt@adobe.com>
2024-08-30 22:36:38 +0530

    add proxy component in allowlist for property sheet

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -96,7 +96,7 @@ function annotateFormFragment(fragmentFieldWrapper, fragmentDefinition) {
 }
 
 function getPropertyModel(fd) {
-  if (!fd[':type'] || fd[':type'].startsWith('core/fd/components') || fd[':type'] === 'wizard') {
+  if (!fd[':type'] || fd[':type'].startsWith('core/fd/components') || fd[':type'].startsWith('formsninja/components/adaptiveForm') || fd[':type'] === 'wizard') {
     return fd.fieldType === 'image' || fd.fieldType === 'button' ? `form-${fd.fieldType}` : fd.fieldType;
   }
   return fd[':type'];
