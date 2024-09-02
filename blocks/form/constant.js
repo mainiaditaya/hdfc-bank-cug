3ca2265c2568ce12e59361d9d2344f7196e7202e
Vaibhav Singh <svaibhav184@gmail.com>
2024-09-02 11:33:34 +0530

    create new folder structure

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -22,4 +22,5 @@ export const formIdPathMapping = {
   '/content/forms/af/hdfc_haf/cards/corporatecreditcard/uat/hdfc': '../../../creditcards/corporate-creditcard/cc-functions.js', // cc
   '/content/forms/af/hdfc_haf/cards/fdlien/forms/fdlien-dev': '../../../creditcards/fd-card/fd-functions.js', // fd
   '/content/forms/af/hdfc_haf/cards/fdlien/forms/copy/fdlien-dev21': '../../../creditcards/fd-card/fd-functions.js', // fd
+  '/content/forms/af/hdfc_haf/liabilities/nre-nro/forms/account-opening-nre-nro': '../../../liabilities/nre-Nro/nre-nro.js', // nre-Nro
 };
