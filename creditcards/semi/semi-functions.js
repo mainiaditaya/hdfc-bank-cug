422e2edd99a53b4f597ef956ccfbf56c6da891f6
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-09-02 19:34:50 +0530

    semi-flicker-fix in css

--- creditcards/semi/semi-functions.js
+++ creditcards/semi/semi-functions.js
@@ -17,14 +17,6 @@ import {
 } from './smart-emi-functions.js';
 import { invokeJourneyDropOff, invokeJourneyDropOffByParam, invokeJourneyDropOffUpdate } from '../../common/journey-utils.js';
 
-/* load SEMI Styles- for loading semi - styles - temporary fix */
-async function loadSEMIStyles() {
-  if (document.querySelector('.semi-form-wrapper')) {
-    document.body.classList.add('semi-form');
-  }
-}
-window.setTimeout(() => loadSEMIStyles(), 600);
-
 export {
   getOTPV1,
   otpValV1,
