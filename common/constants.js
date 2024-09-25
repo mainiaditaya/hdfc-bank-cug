e4afc31b9706c185054a3a7e0dfe4a0188f30903
kunal-adobe <kunalj.adobe@gmail.com>
2024-09-25 10:58:51 +0530

    updated panValidation endpoint url

--- common/constants.js
+++ common/constants.js
@@ -15,7 +15,7 @@ const ENDPOINTS = {
   journeyDropOffUpdate: '/content/hdfc_commonforms/api/journeydropoffupdate.json',
   otpGen: '/content/hdfc_haf/api/otpgenerationccV4.json',
   otpValFetchAssetDemog: '/content/hdfc_haf/api/otpvaldemogV4.json',
-  panValNameMatch: '/content/hdfc_haf/api/panValidation.json',
+  panValNameMatch: '/content/hdfc_forms_common_v2/api/panValNameMatch.json',
   docUpload: '/content/hdfc_etb_wo_pacc/api/documentUpload.json',
   aadhaarCallBack: {
     CORPORATE_CARD_JOURNEY: '/content/hdfc_etb_wo_pacc/api/aadharCallback.json',
