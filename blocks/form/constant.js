8ec4e6204fb6c78bc985ff10f24523bca3a995b2
mainiaditaya <mainiaditaya@gmail.com>
2024-10-17 18:17:12 +0530

    Update constant.js

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -8,7 +8,7 @@ export const defaultErrorMessages = {
   minItems: 'Specify a number of items equal to or greater than $0.',
 };
 
-let submitBaseUrl = '';
+let submitBaseUrl = 'https://hdfc-dev-04.adobecqms.net';
 
 const localDev = ['aem.live', 'aem.page', 'localhost', 'hlx.live', 'hlx.page'];
 
